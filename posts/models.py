23a72ae9b6f66cd952578848ba0d8a697d10aee8
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-10-08 18:42:25 -0400

    added post model

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from secrets import choice
+from xmlrpc.client import DateTime
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    options = (("draft", "Draft"), ("published", "Published"))
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=options, default="published")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
